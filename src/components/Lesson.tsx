1301660320b3a7bb1e42ea0b9eaf711d2adaf2d1
darinformax@gmail.com <darinformax@gmail.com>
2022-06-22 17:53:57 -0300

    adicionada biblioteca 'date-fns' e terminada pagina lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,6 @@
 import { CheckCircle, Lock } from 'phosphor-react';
+import { isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 interface LessonProps {
     title: string;
@@ -7,23 +9,37 @@ interface LessonProps {
     type: 'live' | 'class';
 }
 
-export function Lesson(props: LessonProps) {
+export function Lesson({availableAt, type, title}: LessonProps) {
+
+    const isLessonAvailable = isPast(availableAt);
+    const availableDateFormated = format(availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR
+    });
+
     return (
         <a href="#">
             <span className="text-gray-300">
-                {props.availableAt.toString()}
+                {availableDateFormated}
             </span>
             <div className="rounded border border-gray-600 p-4 mt-4">
                 <header className="flex justify-between items-center ">
-                    <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-                        <CheckCircle size={20} />
-                        Conteúdo liberado
-                    </span>
+                    {isLessonAvailable ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
+                    
                     <span className="text-xs font-bold text-white py-[0.125rem] px-2 rounded border border-green-300 block">
-                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                        {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
                 </header>
-                <strong className="mt-4 block">{props.title}</strong>
+                <strong className="mt-4 block">{title}</strong>
             </div>
         </a>
     )
