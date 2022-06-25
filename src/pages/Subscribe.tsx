951212227fd8ebdd3c08eaceba495d388b668ff3
daryud <darinformax@gmail.com>
2022-06-25 15:06:19 -0300

    importando imagem da pagina subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
+import codeMockupImage from "../assets/code-mockup.png";
+
 export function Subscribe() {
     const navigate = useNavigate();
 
@@ -70,7 +72,7 @@ export function Subscribe() {
                 </div>
             </div>
 
-            <img src="/src/assets/code-mockup.png" alt="code-mockup" className="mt-10 max-w-[1100px]"/>
+            <img src={codeMockupImage} alt="code-mockup" className="mt-10 max-w-[1100px]"/>
         </div>
     )
 }
